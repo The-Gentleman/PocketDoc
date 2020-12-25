ccb0a63cdac8146f6b1098ce5a3821023340e349
The-Gentleman <zraja4402@gmail.com>
2020-12-25 04:37:46 -0500

    added strong params and create action

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::ExercisesController < ApplicationController
         exercises = Exercise.all 
         render json: ExerciseSerializer.new(exercises)
     end 
+
+    def create
+        exercise = Exercise.new(exercise_params)
+        if exercise.save 
+            render json: ExerciseSerializer.new(exercise), status: :accepted 
+        else 
+            render json: {errors: exercise.errors.full_messages}, status: :unprocessible_entity 
+            # unprocessible_entity is a 402 error saying that syntax is okay, 
+            # but you probably failed some validations
+        end 
+    end 
+
+
+    private
+    def exercise_params
+        params.require(:exercise).permit(:name, :reps, :patient_id)
+    end
 end
