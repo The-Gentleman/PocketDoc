0c3c4846c7a11944b0bc8d1a89d4c15fffd52734
The-Gentleman <zraja4402@gmail.com>
2020-12-25 03:44:58 -0500

    added strong params + create action

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::PatientsController < ApplicationController
         patients = Patient.all 
         render json: patients 
     end 
+
+    def create
+        patient = Patient.new(patient_params)
+        if patient.save 
+            render json: patient, status: :accepted
+        else 
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity 
+            # unprocessible_entity is a 402 error saying that syntax is okay, 
+            # but you probably failed some validations
+        end 
+    end 
+
+
+    private
+    def patient_params
+        params.require(:patient).permit(:name, :diagnosis)
+    end 
 end
