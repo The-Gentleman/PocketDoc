899635d78bc3b1042afc7c5b343924e9fb3f481a
The-Gentleman <zraja4402@gmail.com>
2020-12-29 14:14:06 -0500

    using just one patient

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,42 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    getPatients();
-    exerciseButtons();
+    getPatient()
 })
-
-function getPatients(){
+// asycn await
+function getPatient(){
     const patientEndPoint = 'http://localhost:3000/api/v1/patients'
     // 1 of 3
     fetch(patientEndPoint)
         .then(response => response.json())
-        .then(patients => {
-             patients.data.forEach(patient => {
+        .then(patient => {
                 const patientInfo = `
-                <h2>Patient Name: ${patient.attributes.name}</h2>
-                <h3>Patient Diagnosis: ${patient.attributes.diagnosis}</h3>
+                <div id="patient-container">
+                <h2>Patient Name: ${patient.data[0].attributes.name}</h2>
+                <h3>Patient Diagnosis: ${patient.data[0].attributes.diagnosis}</h3>
                 <button id="exercise-button"type="submit" class="btn btn-secondary">Assign an Exercise</button>
-                <br><br>
+                </div>
                 `
-        document.querySelector('#patient-container').innerHTML += patientInfo;
-
-            })
+        document.querySelector('#patient-container').innerHTML += patientInfo
+        document.getElementById("exercise-button").addEventListener("click", exerciseForm);
         })
 }
-// all the assign exercise buttons ---> document.querySelectorAll("#exercise-button")
-function exerciseButtons(){
-   buttons = document.querySelectorAll("#exercise-button")
-   buttons.forEach(button => {
-       button.addEventListener("click", exerciseForm )
-   })
-   
-}
 
-function exerciseForm(){
-    debugger
+function exerciseForm(event){
+    event.preventDefault()
+    const exerciseForm = `
+    <br>
+      <label>Exercise Name:</label>
+      <input id='input-title' type="text" name="title" value=""
+      placeholder="Exercise name" class="input-text" required>
+      <br><br>
+      <label>Number of Reps:</label>
+      <input type="number" id="reps" name="reps" min="1" max="50"
+      placeholder="reps" class="input-number" required>
+      <br><br>
+      <input id="submit" type="submit" name="submit" value="Create Exercise" class="submit">
+      <br><br>
+    </form>
+  `
+  document.querySelector('#exercise-form-container').innerHTML += exerciseForm
 }
 
