77c3ac069538ba8563867747ce03ec51887a817e
The-Gentleman <zraja4402@gmail.com>
2020-12-30 13:03:03 -0500

    added exercise form handler with prevent default

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    getPatient()
+    getPatient();
 })
 
 function getPatient(){
@@ -15,7 +15,7 @@ function getPatient(){
                 <button id="exercise-button"type="submit" class="btn btn-secondary">Assign an Exercise</button>
                 </div>
                 `
-        document.querySelector('#patient-container').innerHTML += patientInfo
+        document.querySelector('#patient-container').innerHTML += patientInfo;
         document.getElementById("exercise-button").addEventListener("click", exerciseForm);
         })
 }
@@ -24,23 +24,38 @@ function exerciseForm(){
     const exerciseForm = `
     <br>
     <form id="create-exercise-form">
+      <label>Patient Name:</label>
+      <select id="exercises" name="exercises">
+      <option value="1">Ted</option>
+      </select>
+      <br><br>
       <label>Exercise Name:</label>
       <input id='input-title' type="text" name="title" value=""
-      placeholder="Exercise name" class="input-text" required>
+      placeholder="Exercise name" class="input-text">
       <br><br>
       <label>Number of Reps:</label>
       <input type="number" id="reps" name="reps" min="1" max="50"
-      placeholder="reps" class="input-number" required>
+      placeholder="reps" class="input-number">
       <br><br>
       <input id="submit" type="submit" name="submit" value="Create Exercise" class="submit">
+      <br><br>
     </form>
-  `
-  document.querySelector('#exercise-form-container').innerHTML += exerciseForm
-  document.querySelector("#create-exercise-form").addEventListener("submit", (e) => exerciseFormHandler(e))
+  `  
+  document.querySelector('#exercise-form-container').innerHTML += exerciseForm;
+  document.querySelector("#create-exercise-form").addEventListener("submit", (e) => exerciseFormHandler(e));
 }
 
 function exerciseFormHandler(e){
     e.preventDefault(e);
-    console.log(e)
+    const patientValueString = document.querySelector("#exercises").value
+    const patientValue = parseInt(patientValueString);
+    const exerciseName =  document.querySelector("#input-title").value;
+    const numberOfRepsString = document.querySelector("#reps").value;
+    const numberOfReps = parseInt(numberOfRepsString);
+    exerciseFetch(exerciseName, numberOfReps, patientValue);
+}
+
+function exerciseFetch(name, reps, patient_id){
+
 }
 
