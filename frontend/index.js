b4c325d6b5fd357c284491eac406cc85bad054b3
The-Gentleman <zraja4402@gmail.com>
2020-12-31 03:13:19 -0500

    added delete function. exercise ID still returns undefined

--- frontend/index.js
+++ frontend/index.js
@@ -10,9 +10,11 @@ function getPatient(){
         .then(patient => {
                 const patientInfo = `
                 <div id="patient-container">
-                <h2>Patient Name: ${patient.data[0].attributes.name}</h2>
-                <h3>Patient Diagnosis: ${patient.data[0].attributes.diagnosis}</h3>
-                <button id="exercise-button"type="submit" class="btn btn-secondary">Assign an Exercise</button>
+                    <h2>Patient Name: ${patient.data[0].attributes.name}</h2>
+                    <h3>Patient Diagnosis: ${patient.data[0].attributes.diagnosis}</h3>
+                    <div class="exercise-button-container">
+                        <button id="exercise-button"type="submit" class="btn btn-secondary">Assign an Exercise</button>
+                    </div>
                 </div>
                 `
         document.querySelector('#patient-container').innerHTML += patientInfo;
@@ -26,18 +28,18 @@ function exerciseForm(){
     <form id="create-exercise-form">
       <label>Patient Name:</label>
       <select id="exercises" name="exercises">
-      <option value="1">Ted</option>
+        <option value="1">Ted</option>
       </select>
       <br><br>
       <label>Exercise Name:</label>
       <input id='input-title' type="text" name="title" value=""
-      placeholder="Exercise name" class="input-text">
+      placeholder="Exercise name" class="input-text" required>
       <br><br>
       <label>Number of Reps:</label>
       <input type="number" id="reps" name="reps" min="1" max="50"
-      placeholder="reps" class="input-number">
+      placeholder="reps" class="input-number" required>
       <br><br>
-      <input id="submit" type="submit" name="submit" value="Create Exercise" class="submit">
+        <input id="submit" type="submit" name="submit" value="Create Exercise" class="submit">
       <br><br>
     </form>
   `  
@@ -68,17 +70,35 @@ function exerciseFetch(name, reps, patient_id){
     .then(exercises =>{
         const exerciseData = exercises.data.attributes;
         const exerciseMarkup = `
-        <div data-id=${exercises.id}>
-        <h3>Exercise Name: ${exerciseData.name}</h3>
-        <h3>Number of Reps: ${exerciseData.reps}</h3>
-        </div>
-        <div class="delete-button">
-        <button id="delete-exercise-button" type="delete" class="btn btn-secondary">Delete Exercise</button>
+            <input type="hidden" id="exercisesID" name="exercisesID" value="${exercises._id}">
+            <h3>Exercise Name: ${exerciseData.name}</h3>
+            <h3>Number of Reps: ${exerciseData.reps}</h3>
+            <button id="delete-exercise-button" type="delete" class="btn btn-secondary">Delete Exercise</button>
         </div>
         `
         document.querySelector("#exercise-data-container").innerHTML += exerciseMarkup;
-    })
+        document.querySelector("#delete-exercise-button").addEventListener("click", (event) => deleteExercise(event));
 
+    })
 }
+
+function deleteExercise(event){
+    const exerciseEndpoint = 'http://localhost:3000/api/v1/exercises';
+    // const bodyData = {name, reps, patient_id};
+    let deletePost = event.target.id == "delete-exercise-button";
+    let exerciseId = document.getElementById("exercisesID").value;
+    if (deletePost){
+        console.log(exerciseId)
+        // document.querySelector("#exercise-data-container").children has a div called dataset where exercise ID is supposed to be
+    }
+//     // 3 of 3
+}
+
+// ===================================================================================================
+// PLAN A:
+// DELETE FETCH REQUEST 
+// PLAN B:
+// GET FETCH REQUEST FOR EXERCISES
+// ===================================================================================================
 
 
