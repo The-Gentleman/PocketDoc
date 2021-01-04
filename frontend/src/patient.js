cf974f624be3a46533707f8223a2eff04ec9441c
The-Gentleman <zraja4402@gmail.com>
2021-01-04 02:10:04 -0500

    constructor is functional

--- frontend/src/patient.js
+++ frontend/src/patient.js
@@ -1,3 +1,8 @@
 class Patient {
+    constructor(patient, patientAttributes){
+        this.id = patient.id
+        this.name = patientAttributes.name
+        this.diagnosis = patientAttributes.diagnosis
 
+    }
 }
