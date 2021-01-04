30ad40eefe51f458cbc5b4d22a8895436be595fd
The-Gentleman <zraja4402@gmail.com>
2021-01-04 02:12:27 -0500

    added Patient.all

--- frontend/src/patient.js
+++ frontend/src/patient.js
@@ -3,6 +3,7 @@ class Patient {
         this.id = patient.id
         this.name = patientAttributes.name
         this.diagnosis = patientAttributes.diagnosis
-
+        Patient.call.push(this)
     }
 }
+Patient.all = []
