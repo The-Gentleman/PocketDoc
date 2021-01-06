f8835380e1502cf972a95f8096df8b03d7b0f3c5
The-Gentleman <zraja4402@gmail.com>
2021-01-05 23:38:35 -0500

    refactored getPatient function into Patient class

--- frontend/src/patient.js
+++ frontend/src/patient.js
@@ -5,5 +5,33 @@ class Patient {
         this.diagnosis = patientAttributes.diagnosis
         Patient.all.push(this)
     }    
+
+    static getPatient(){
+        const patientEndPoint = 'http://localhost:3000/api/v1/patients'
+        // 1 of 3
+        fetch(patientEndPoint)
+            .then(response => response.json())
+            .then(patient => {
+                const patientAttributes = patient.data
+                patientAttributes.forEach(patient => {            
+                    const newPatient = new Patient(patient, patient.attributes)
+                    const patientInfo = `
+                    <div id="patient-container">
+                    <input type="hidden" id="patient-id" name="patient-id" value="${patient.id}">
+                    <h2>Patient Name: ${patient.attributes.name}</h2>
+                    <h3>Patient Diagnosis: ${patient.attributes.diagnosis}</h3>
+                    <div class="exercise-button-container">
+                    <button id="exercise-button"type="submit" class="btn btn-secondary">Assign an Exercise</button>
+                    </div>
+                    </div>
+                    `
+                    document.querySelector('#patient-container').innerHTML += patientInfo;
+                    document.getElementById("exercise-button").addEventListener("click", Exercise.exerciseForm);
+            
+        })
+            })
+        }
+        
+    
 }
 Patient.all = []
