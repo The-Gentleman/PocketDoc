30433cf8864e1d8a7856d8924dbeafa6533333a2
The-Gentleman <zraja4402@gmail.com>
2021-09-17 03:08:25 -0400

    added a readme

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,6 +2,7 @@ ted = Patient.create(name: "Ted", diagnosis: "sprained neck")
 marshal = Patient.create(name: "Marshall", diagnosis: "broken hip")
 barney = Patient.create(name: "Barney", diagnosis: "broken hand")
 alex = Patient.create(name: "Alex", diagnosis: "sore throat")
+zeshan = Patient.create(name: "Zeshan", diagnosis: "overworked and underpaid")
 
 Exercise.create(name: "Push ups", reps: 5, patient_id: ted.id)
 Exercise.create(name: "Pull ups", reps: 10, patient_id: ted.id)
